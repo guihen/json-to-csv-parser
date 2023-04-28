966591e0d5b511c9117ac37e798e5ed7d1da8222
Guilherme Rojas <guilhermehrojas@gmail.com>
2023-04-28 14:36:45 -0300

    Generates a CSV file using the sample data.
    Each speaker will be written in a new line.
    
    Co-authored-by: Beatriz Andrade (beandy.023@gmail.com)

--- json_to_csv_parser.rb
+++ json_to_csv_parser.rb
@@ -1,15 +1,23 @@
 require 'json'
+require 'csv'
 
-file = File.open('api-response-1.json')
+file = File.open('api-response-2.json')
 json = JSON.parse(file.read)
 
-csv_header = %(talk data_hora palestrante cargo empresa foto talk_keywords)
+
+csv_file = File.open('websummit.csv', 'w')
+csv_file.write CSV.generate_line(%w(talk data_hora palestrante cargo empresa foto talk_keywords))
 
 talk_event = json["results"][0]["hits"].first
 talk = talk_event["title"]
 data_hora = talk_event["starts_at"]
 participants = talk_event["timeslot_participations"]
-palestrante = participants.first["name"]
-cargo = participants.first["job_title"]
-empresa = participants.first["company_name"]
-foto = participants.first["avatar_urls"]["thumb"]
+
+participants.each do |participant|
+  palestrante = participant["name"]
+  cargo = participant["job_title"]
+  empresa = participant["company_name"]
+  foto = participant["avatar_urls"]["thumb"]
+
+  csv_file.write CSV.generate_line([talk, data_hora, palestrante, cargo, empresa, foto, nil])
+end
